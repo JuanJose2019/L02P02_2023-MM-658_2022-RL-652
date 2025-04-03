19df7070252f5f596bd65f2e26ff8ef06bc42979
Juan José <juanjosemagaa@hotmail.com>
2025-04-03 17:11:21 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2023-MM-658_2022-RL-652/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using L02P02_2023_MM_658_2022_RL_652.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace L02P02_2023_MM_658_2022_RL_652.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ L02P02_2023-MM-658_2022-RL-652/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2023_MM_658_2022_RL_652.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
