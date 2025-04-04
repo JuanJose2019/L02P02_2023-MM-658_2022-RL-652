c4a859f6dfbaa1302cc09e498547c92f42a90199
7oswaldo7 <oswaldoretana05@gmail.com>
2025-04-03 19:17:38 -0600

    cambios en el controllador autores

--- L02P02_2023-MM-658_2022-RL-652/Controllers/autoresController.cs
+++ L02P02_2023-MM-658_2022-RL-652/Controllers/autoresController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using L02P02_2023_MM_658_2022_RL_652.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace L02P02_2023_MM_658_2022_RL_652.Controllers
 {
     public class autoresController : Controller
+
+
     {
+        private readonly libreriaDbContext _context;
+        public autoresController(libreriaDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var autores = _context.autores.ToList();
+            return View(autores);
         }
     }
 }
+
+
